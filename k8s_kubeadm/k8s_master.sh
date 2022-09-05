43db67f242d871b7b7168b2c06553713fd396e9e
Vyacheslav <nodegopher@gmail.com>
2022-09-05 12:34:00 +0300

    fixed

--- k8s_kubeadm/k8s_master.sh
+++ k8s_kubeadm/k8s_master.sh
@@ -25,25 +25,46 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-sysctl --system
-apt-get update
-apt-get -y install containerd
+wget https://github.com/containerd/containerd/releases/download/v1.6.2/containerd-1.6.2-linux-amd64.tar.gz
+tar Cxzvf /usr/local containerd-1.6.2-linux-amd64.tar.gz
+systemctl daemon-reload
+systemctl enable --now containerd
+
+echo "........................Runc........................"
+
+wget https://github.com/opencontainers/runc/releases/download/v1.1.1/runc.amd64
+install -m 755 runc.amd64 /usr/local/sbin/runc
+
+echo "........................CNI_plugins........................"
+
+sudo mkdir -p /opt/cni/bin/
+sudo wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+echo "........................Config_for_k8s........................"
 
 mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
-sed '/^SystemdCgroup = true before=[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]' test.txt /etc/containerd/config.toml
-
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 systemctl restart containerd
+
+#cat <<EOF | sudo tee -a /etc/containerd/config.toml
+#[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+#[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+#SystemdCgroup = true
+#EOF
+#sudo sed -i 's/^disabled_plugins \=/\#disabled_plugins \=/g' /etc/containerd/config.toml
+#sudo systemctl restart containerd
 
 echo "........................Kubeadm........................"
 
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 systemctl enable kubelet
 kubeadm init --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
