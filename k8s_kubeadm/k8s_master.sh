8570878c2b673c61aa80e8ee7d58cb9476e58233
Vyacheslav <nodegopher@gmail.com>
2022-09-08 22:47:33 +0300

    fixed

--- k8s_kubeadm/k8s_master.sh
+++ k8s_kubeadm/k8s_master.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo hostnamectl set-hostname "k8smaster.example.net"
-exec bash
 apt update && apt upgrade
 
 echo "........................Swap........................"
@@ -46,7 +45,8 @@ sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-sudo kubeadm init --control-plane-endpoint=k8smaster.example.net --skip-phases=addon/kube-proxy
+
+kubeadm init --skip-phases=addon/kube-proxy --control-plane-endpoint=k8smaster.example.net
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -58,7 +58,7 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 echo "........................Cillium........................"
 
 helm repo add cilium https://helm.cilium.io/
-helm install cilium cilium/cilium --version 1.12.0 --namespace kube-system
+helm install cilium cilium/cilium --version 1.12.1 --namespace kube-system
 
 CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
 CLI_ARCH=amd64
@@ -67,8 +67,6 @@ curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/d
 sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
 sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
 rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
-
-sleep 200s
 
 kubectl get nodes
 cilium status
