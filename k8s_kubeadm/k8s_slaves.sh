0d2402ebd2d2984a6c05ed66a331865c31445ac5
Vyacheslav <nodegopher@gmail.com>
2022-09-11 16:13:54 +0300

    fixed

--- k8s_kubeadm/k8s_slaves.sh
+++ k8s_kubeadm/k8s_slaves.sh
@@ -53,4 +53,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt update
 sudo apt install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+
+#   sudo apt-mark hold kubelet kubeadm kubectl
