74bc13f2a52016d4e17a03cb69d82c27c4e1c5f0
Vyacheslav <nodegopher@gmail.com>
2022-09-19 20:44:43 +0300

    fixed

--- k8s_kubeadm/k8s_slaves.sh
+++ k8s_kubeadm/k8s_slaves.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# yes | sudo bash k8s_slaves.sh
+
 # sudo hostnamectl set-hostname "k8sworker1.example.net"
 # exec bash
-# yes | sudo bash k8s_slaves.sh
+# sudo apt update
+# sudo apt -y full-upgrade
+# [ -f /var/run/reboot-required ] && sudo reboot -f
 
 cat <<EOT >>/etc/hosts
 192.168.1.110 k8smaster.example.net k8smaster
@@ -10,48 +14,55 @@ cat <<EOT >>/etc/hosts
 192.168.1.112 k8sworker2.example.net k8sworker2
 EOT
 
-apt update && apt upgrade
+
+echo "........................kubelet, kubeadm, kubectl........................"
+sudo apt install curl wget apt-transport-https -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update
+sudo apt install kubelet kubeadm kubectl -y
+sudo apt-mark hold kubelet kubeadm kubectl
+
 
 echo "........................Swap........................"
-
 sudo swapoff -a
 sed -i '/swap/s/^/#\ /' /etc/fstab
 
-echo "........................Config........................"
 
+echo "........................Config........................"
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
-
 modprobe overlay
 modprobe br_netfilter
-
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
-
 sudo sysctl --system
 
-echo "........................Contenerd........................"
 
+echo "........................Contenerd........................"
 sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y containerd.io
-containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+mkdir -p /etc/containerd
+containerd config default>/etc/containerd/config.toml
+# containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo systemctl enable containerd
+sudo systemctl enable kubelet
 
-echo "........................k8s........................"
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-sudo apt update
-sudo apt install -y kubelet kubeadm kubectl
+# sudo apt-mark hold kubelet kubeadm kubectl
 
-#   sudo apt-mark hold kubelet kubeadm kubectl
+
+
+
+
+
