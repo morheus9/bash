830595371e0773b993d814e5a27a8eca0cb3635d
Vyacheslav <nodegopher@gmail.com>
2022-09-20 17:01:40 +0300

    fixed

--- k8s_kubeadm/cilium.sh
+++ k8s_kubeadm/cilium.sh
@@ -3,9 +3,7 @@
 # yes | sudo bash cilium.sh
 
 echo "........................Cilium_without_kubeproxy........................"
-
 helm repo add cilium https://helm.cilium.io/
-
 API_SERVER_IP=192.168.1.110
 API_SERVER_PORT=6443
 helm install cilium cilium/cilium --version 1.12.1 \
@@ -15,7 +13,6 @@ helm install cilium cilium/cilium --version 1.12.1 \
 --set k8sServicePort=${API_SERVER_PORT}
 
 echo "........................Cilium_CLI........................"
-
 CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
 CLI_ARCH=amd64
 if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
@@ -25,7 +22,6 @@ sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
 rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
 
 echo "........................Cilium_ingress........................"
-
 helm upgrade cilium cilium/cilium --version 1.12.1 \
 --namespace kube-system \
 --reuse-values \
--- k8s_kubeadm/k8s_master.sh
+++ k8s_kubeadm/k8s_master.sh
@@ -7,50 +7,37 @@
 # sudo apt update
 # sudo apt -y full-upgrade
 
-
 cat <<EOT >>/etc/hosts
 192.168.1.110 k8smaster.example.net k8smaster
 192.168.1.111 k8sworker1.example.net k8sworker1
 192.168.1.112 k8sworker2.example.net k8sworker2
 EOT
 
-
 echo "........................Swap........................"
 sudo swapoff -a
 sed -i '/swap/s/^/#\ /' /etc/fstab
-
 
 echo "........................Contenerd........................"
 cd /tmp/
 wget https://github.com/containerd/containerd/releases/download/v1.6.8/containerd-1.6.8-linux-amd64.tar.gz
 tar Cxzvf /usr/local containerd-1.6.8-linux-amd64.tar.gz
 
-
 echo "........................Contenerd_service........................"
 wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
 cp containerd.service /etc/systemd/system
 
-
 echo "........................Runc........................"
 wget https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.amd64
 install -m 755 runc.amd64 /usr/local/sbin/runc
-
 
 echo "........................Cni_plugins........................"
 wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
 mkdir -p /opt/cni/bin
 tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
 
-# sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-# sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
-# sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-# sudo apt update
-# sudo apt install -y runc containerd.io
-
 # kubectl edit cm kubelet-config -n kube-system
 # cgroupDriver: systemd
 # /var/lib/kubelet/config.yaml
-
 
 echo "........................Config........................"
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
@@ -65,14 +52,11 @@ net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 sudo sysctl --system
-
 mkdir -p /etc/containerd
 containerd config default>/etc/containerd/config.toml
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-
 systemctl daemon-reload
 systemctl enable --now containerd
-
 
 echo "........................Kubelet, kubeadm, kubectl........................"
 sudo apt install curl wget apt-transport-https -y
@@ -82,7 +66,6 @@ sudo apt update
 sudo apt install kubelet kubeadm kubectl -y
 sudo apt-mark hold kubelet kubeadm kubectl
 systemctl enable --now kubelet
-
 
 echo "........................Init........................"
 sudo kubeadm config images pull --cri-socket /run/containerd/containerd.sock
@@ -94,7 +77,6 @@ sudo kubeadm init \
    --control-plane-endpoint=k8smaster.example.net
 #  --skip-phases=addon/kube-proxy \
 
-
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -102,6 +84,6 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 echo "........................Helm3........................"
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
-kubectl get nodes
 kubeadm token create --print-join-command
-
+kubectl get nodes
+kubectl get pods -n kube-system
