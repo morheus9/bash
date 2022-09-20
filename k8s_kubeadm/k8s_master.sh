ad86beb83716c3f37d1d18e8ec7f07e9c176636e
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-09-20 10:08:38 +0300

    Update k8s_master.sh

--- k8s_kubeadm/k8s_master.sh
+++ k8s_kubeadm/k8s_master.sh
@@ -63,7 +63,7 @@ sudo kubeadm init \
    --upload-certs \
    --control-plane-endpoint=k8smaster.example.net
 #  --skip-phases=addon/kube-proxy \
-№  --pod-network-cidr=10.244.0.0/16 \
+#  --pod-network-cidr=10.244.0.0/16 \
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
