080baa6d98841a6d63ddd64814e4b1abbe16b84b
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-10-12 12:50:11 +0300

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -13,5 +13,13 @@ pg_dump -U $dbUser $database | gzip >$pathB/pgsql_$(date "+%Y-%m-%d").sql.gz
 
 unset PGPASSWORD
 
-# crontab -e
-# 3 0 * * * /etc/scripts/pgsql_dump.sh # postgres pg dump
+#________________________all_DB________________________#
+
+#!/bin/bash
+# PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+# pathB=/backup/postgres
+
+# find $pathB \( -name "*-1[^5].*" -o -name "*-[023]?.*" \) -ctime +61 -delete
+# for dbname in $(echo "SELECT datname FROM pg_database;" | psql | tail -n +3 | head -n -2 | egrep -v 'template0|template1|postgres'); do
+#     pg_dump $dbname | gzip >$pathB/$dbname-$(date "+%Y-%m-%d").sql.gz
+# done
