43f94591151ca874b4106e40522ed21415ef9263
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-11-02 18:08:13 +0300

    Update ansible_ubuntu+debian.sh

--- my_os/ansible_ubuntu+debian.sh
+++ my_os/ansible_ubuntu+debian.sh
@@ -5,5 +5,6 @@ apt update && apt upgrade
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+pip3 install "ansible-lint"
 
 ansible --version
