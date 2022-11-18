ba3bb1ae8db3974880d93381fdc7426ae47b05d1
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-11-18 23:55:56 +0300

    Update ubuntu.sh

--- my_os/ubuntu.sh
+++ my_os/ubuntu.sh
@@ -3,7 +3,7 @@
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt install ttf-mscorefonts-installer
 #Update
-sudo apt update && sudo apt full-upgrade
+sudo apt update && sudo apt upgrade
 #Kodecs+Gcc+Nvidia
 sudo apt install ubuntu-restricted-extras libavcodec-extra
 sudo apt -y install build-essential
@@ -14,7 +14,7 @@ sudo snap install libreoffice
 sudo snap install foliate
 #Git
 sudo apt install git
-sudo apt update
-sudo apt upgrade
+sudo apt update && sudo apt upgrade
+
 
 # yes | sudo bash ubuntu.sh
