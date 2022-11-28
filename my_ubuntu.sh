7352acca747a6a738ef529d2acc0aabb815cd9d2
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-11-28 21:33:18 +0300

    Update my_ubuntu.sh

--- my_ubuntu.sh
+++ my_ubuntu.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
-sudo apt install ttf-mscorefonts-installer
 # Update
 sudo apt update && sudo apt upgrade
 # Kodecs+Gcc+Nvidia
