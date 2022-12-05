459fc619be18bbe16e97b0f5da118a41d0e51487
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2022-12-05 18:29:13 +0300

    Update my_ubuntu.sh

--- my_ubuntu.sh
+++ my_ubuntu.sh
@@ -6,17 +6,14 @@ sudo apt update && sudo apt upgrade
 sudo apt install ubuntu-restricted-extras libavcodec-extra
 sudo apt -y install build-essential
 sudo ubuntu-drivers autoinstall
-# IDE
-sudo snap install code --classic
-sudo snap install libreoffice
-sudo snap install foliate
 # Git
 sudo apt install git
 # Ansible
-sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+python3 -m pip install --user ansible
 pip3 install "ansible-lint"
-
+# Vargant
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant
 # ansible --version
 # yes | sudo bash ubuntu.sh
