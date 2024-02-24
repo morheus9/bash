b11fc6c6eabde83b8a2baa2a8e2e09159ee9e901
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2024-02-24 18:19:18 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,1 +1,12 @@
-pacman -Qm - найти пакеты аур
+sudo pacman -Sy - обновить СПИСОК пакетов
+sudo pacman -Syyu - обновить все пакеты
+yay -Syu - обновить все пакеты (+aur)
+
+sudo pacman -S foo - установить пакет
+yay -S foo - установить пакет
+
+sudo pacman -Rns foo - удалить пакет и зависимости
+yay -Rns foo - удалить пакет и зависимости
+
+sudo pacman -Qm - найти пакеты aur
+yay -Yc - найти ненужные зависимости
