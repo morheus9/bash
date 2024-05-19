dac2c2e0d2f5723f2ea61462ed4a8689006d16d4
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2024-05-19 12:37:59 +0300

    Create ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,24 @@
+# yes | sudo bash ubuntu.sh
+# Update
+sudo echo 'alias upg="sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc \
+&& source ~/.bashrc \
+&& upg
+# Kodecs+Gcc
+sudo apt install ubuntu-restricted-extras libavcodec-extra \
+&& sudo apt -y install build-essential -y
+# Git, transmission, timeshift and virtualbox
+sudo apt install git \
+&& transmission \
+&& timeshift \
+&& virtualbox -y
+# Go
+sudo snap install go -y
+# Python
+sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update
+sudo apt install python3.12 python3.12-venv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+# SSH
+ssh-keygen -t ed25519
+chmod 700 ~/.ssh/id_ed25519
+ssh-copy-id -i ~/.ssh/id_ed25519.pub -f pi@192.168.1.104
+ssh-copy-id -i ~/.ssh/id_ed25519.pub -f pi@192.168.1.105
