0b096d4c9d791fb658ffdc9e2e34b72f19ebde12
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2024-05-19 21:04:36 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,17 +1,12 @@
 # yes | sudo bash ubuntu.sh
 # Update
-sudo echo 'alias upg="sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc \
-&& source ~/.bashrc \
-&& upg
+sudo echo 'alias upg="sudo snap refresh && sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc && source ~/.bashrc && upg
 # Kodecs+Gcc
-sudo apt install ubuntu-restricted-extras libavcodec-extra \
-&& sudo apt -y install build-essential -y
+sudo apt install ubuntu-restricted-extras libavcodec-extra build-essential -y
 # Git, transmission, timeshift and virtualbox
 sudo apt install git transmission timeshift virtualbox -y
 # SSH
 ssh-keygen -t ed25519
 chmod 700 ~/.ssh/id_ed25519
-# Python
-# sudo add-apt-repository ppa:deadsnakes/ppa && sudo apt update
-# sudo apt install python3.12 python3.12-venv
-# curl -LsSf https://astral.sh/uv/install.sh | sh
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
