50c7563420ac3237adf2cb333db9fc7609f4e623
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2024-09-09 15:07:35 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,6 +5,3 @@ sudo echo 'alias upg="sudo snap refresh && sudo apt update && sudo apt upgrade -
 sudo apt install ubuntu-restricted-extras libavcodec-extra build-essential -y
 # Git, transmission, timeshift and virtualbox
 sudo apt install git transmission timeshift virtualbox -y
-# SSH
-ssh-keygen -t ed25519
-chmod 700 ~/.ssh/id_ed25519
