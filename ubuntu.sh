908556eed1e6dda0552a45f50ba1f5ed545715a1
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2024-09-13 12:52:43 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,7 +1,26 @@
 # yes | sudo bash ubuntu.sh
-# Update
-sudo echo 'alias upg="sudo snap refresh && sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc && source ~/.bashrc && upg
-# Kodecs+Gcc
-sudo apt install ubuntu-restricted-extras libavcodec-extra build-essential -y
-# Git, transmission, timeshift and virtualbox
-sudo apt install git transmission timeshift virtualbox -y
+sudo echo 'alias upg="sudo snap refresh && sudo apt update && sudo apt upgrade -y"' >> ~/.bashrc
+source ~/.bashrc
+upg
+
+wget -c -O code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+wget -c -O docker_desktop.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64"
+
+wget -qO- https://releases.warp.dev/linux/keys/warp.asc | gpg --dearmor > warpdotdev.gpg
+sudo install -D -o root -g root -m 644 warpdotdev.gpg /etc/apt/keyrings/warpdotdev.gpg
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/warpdotdev.gpg] https://releases.warp.dev/linux/deb stable main" > /etc/apt/sources.list.d/warpdotdev.list'
+rm warpdotdev.gpg
+
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install ubuntu-restricted-extras libavcodec-extra build-essential git transmission timeshift vlc virtualbox warp-terminal ./*.deb -y
+curl -sSL https://install.python-poetry.org | python3 -
+rm -f ./*.deb
