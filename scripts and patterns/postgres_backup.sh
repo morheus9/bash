5c873287967cedae8a6e9ec272aac0021d386905
Vyacheslvav <68808419+morheus9@users.noreply.github.com>
2025-12-21 19:14:54 +0300

    Update postgres_backup.sh

--- scripts and patterns/postgres_backup.sh
+++ scripts and patterns/postgres_backup.sh
@@ -1,25 +1,9 @@
+# Set PGPASSWORD=your's_pass_db
+
 #!/bin/sh
 
-PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-
-PGPASSWORD=some_password
-export PGPASSWORD
-pathB=/mnt/backup
-dbUser=dbadmin
-database=zabbix
-
-find $pathB \( -name "*-1[^5].*" -o -name "*-[023]?.*" \) -ctime +61 -delete
-pg_dump -U $dbUser $database | gzip >$pathB/pgsql_$(date "+%Y-%m-%d").sql.gz
-
-unset PGPASSWORD
-
-#________________________all_DB________________________#
-
-#!/bin/bash
-# PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
-# pathB=/backup/postgres
-
-# find $pathB \( -name "*-1[^5].*" -o -name "*-[023]?.*" \) -ctime +61 -delete
-# for dbname in $(echo "SELECT datname FROM pg_database;" | psql | tail -n +3 | head -n -2 | egrep -v 'template0|template1|postgres'); do
-#     pg_dump $dbname | gzip >$pathB/$dbname-$(date "+%Y-%m-%d").sql.gz
-# done
+DATE=$(date '+%Y-%m-%d')
+echo "Saving dump db.."
+pg_dump -h <ip> -p <port> -U <user> -v -F c -f db_$DATE.tar.gz <db name> 2> /opt/backup-db/dump_log_$DATE && [[ $? -eq 0 ]] \
+&& curl -i -X GET "https://api.telegram.org/bot<bot id>:<bot token>/sendMessage?chat_id=<chat id>&text=Дамп базы данных снят успешно, на сервере <servername>" \
+|| curl -i -X GET "https://api.telegram.org/bot<bot id>:<bot token>/sendMessage?chat_id=<chat id>&text=Во время снятия дампа произошла ошибка. Смотри лог на сервере <servername>"
